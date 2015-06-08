c2bf0b47f177decb89b5efbbf90f300bfdfb33ec
Carlos <cverdes@gmail.com>
2015-06-08 21:27:05 +0200

    fix injection

--- client/app/services/replayerService/replayerService.service.js
+++ client/app/services/replayerService/replayerService.service.js
@@ -24,7 +24,7 @@
             };
         }
         
-        function Channel() {
+        function Channel($synthService) {
           
           var generatorsByNote = {};
           var currentProgram = $synthService.PianoProgram; // NOT USED
